ba4bbf4c9cc45af622205f0d466f94908a2102ba
YU-LIN-LIN <nophy0428@gmail.com>
2020-06-03 16:42:00 +0800

    exam03

---
+++ mqtt_client.py
@@ -0,0 +1,45 @@
+import paho.mqtt.client as paho
+import time
+import numpy as np
+import serial
+
+mqttc = paho.Client()
+
+#serdev = '/dev/ttyACM0'
+#s = serial.Serial(serdev,115200)
+
+# Settings for connection
+host = "localhost"
+topic= "velocity"
+port = 1883
+
+# Callbacks
+def on_connect(self, mosq, obj, rc):
+    print("Connected rc: " + str(rc))
+
+def on_message(mosq, obj, msg):
+    print("[Received] Topic: " + msg.topic + ", Message: " + str(msg.payload) + "\n")
+
+def on_subscribe(mosq, obj, mid, granted_qos):
+    print("Subscribed OK")
+
+def on_unsubscribe(mosq, obj, mid, granted_qos):
+    print("Unsubscribed OK")
+
+# Set callbacks
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_subscribe = on_subscribe
+mqttc.on_unsubscribe = on_unsubscribe
+
+# Connect and subscribe
+print("Connecting to " + host + "/" + topic)
+mqttc.connect(host, port=1883, keepalive=60)
+mqttc.subscribe(topic, 0)
+
+while(1):
+    #mesg = "Hello, world!"
+    #mesg = readline()
+    mqttc.publish(topic, mesg)
+    print(mesg)
+    #time.sleep(0.1)
